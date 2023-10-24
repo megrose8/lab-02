fe0485c0b01de5a5be80022a35f723ce24d5e1a0
Meg Breen <mbreen@gicw.org>
2023-10-24 15:24:41 -0700

    updates to questions and fixing case sensitivity

--- js/app.js
+++ js/app.js
@@ -1,49 +1,70 @@
 'use strict'
-//question 1
-let guessEducation = prompt('When do you think I graduated college?')
-    let edu = '2023';
-    console.log(guessEducation)
+//greeting prompts//
+let userName = prompt('Hi there! What should I call you?')
+    //console.log(userName)
 
-if (guessEducation === '2023'){
+alert('Hi there ' + userName + ' !')
+
+//question 1//
+let guessEducation = prompt('When do you think I graduated college? Please answer in YYYY numerical format.')
+    let edu = '2023';
+    //console.log(guessEducation)
+
+if (guessEducation === '2023') {
    alert('Wow! Great guess! It is!');
 } 
 else {
   alert('no not quite...');
 }
 
-//question 2
-let guessEducation = prompt('When do you think I graduated college?')
-    let edu = '2023';
-    console.log(guessEducation)
+//question 2//
+let guessHomeTown = prompt('Where do you think I am from?')
+let homeTown = 'spokane';
+    //console.log(guessHomeTown)
 
-if (guessEducation === '2023'){
-   alert('Wow! Great guess! It is!');
+if (guessHomeTown.toLowerCase() === homeTown ) {
+   alert('Unfortunately, yes. Got the heck out of there...');
 } 
 else {
-  alert('no not quite...');
+  alert('Lower your expectations, you are incorrect');
 }
-//question 3
 
-let guessEducation = prompt('When do you think I graduated college?')
-    let edu = '2023';
-    console.log(guessEducation)
+//question 3//
+let guessPet = prompt('What type of animal do you think I have as a pet?') 
+let pet = 'dog';
+    //console.log(guessPet)
 
-if (guessEducation === '2023'){
-   alert('Wow! Great guess! It is!');
+if (guessPet.toLowerCase() === pet ) {
+   alert('Yes! I love her very much...');
 } 
 else {
-  alert('no not quite...');
+  alert('Nope! Not quite.');
 }
 
-//question 4
-let guessEducation = prompt('When do you think I graduated college?')
-    let edu = '2023';
-    console.log(guessEducation)
+//question 4//
+let guessMajor= prompt('What do you think my college major was?')
+let major = 'history';
+    //console.log(guessMajor)
 
-if (guessEducation === '2023'){
-   alert('Wow! Great guess! It is!');
+if (guessMajor.toLowerCase() === major ) {
+   alert('Yep! And it definitely was worth the student debt...');
 } 
 else {
-  alert('no not quite...');
+  alert('Nope! Think of something less applicable to the employment market.');
 }
+
+//question 5//
+let guessHobby = prompt('Do you think I like to read?')
+let read = 'yes';
+    //console.log(guessHobby)
+
+if (guessHobby.toLowerCase() === read ) {
+   alert('Yep! I just finished Pet Semetary! If you are not into sleeping, I highly recommend it.');
+} 
+else {
+  alert('Incorrect! I love to read.');
+}
+
+//goodbye message//
+alert('Thanks for playing ' + userName + '! Hope you learned some new things about me!' )
 
